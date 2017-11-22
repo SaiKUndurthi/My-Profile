51d3ef5c3c857c7ebc8701361a7e6ae016c10404
SaiKUndurthi <sai.undurthi@gmail.com>
2017-11-22 00:46:48 -0800

    Minor edits to About and Projects page. Also added firebase for hosting.

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ class App extends Component {
                 <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
                   <div className="my-auto">
                     <h2 className="mb-5">Experience</h2>
-                    <Experience title="Software Engineer Intern" companyName="Home Slice" description="Built a new user-facing features website using React JS for HomeSlice customers. Used GraphQL to build API’s.
+                    <Experience title="Software Engineering Intern" companyName="Home Slice" description="Built a new user-facing features website using React JS for HomeSlice customers. Used GraphQL to build API’s.
 Responsible for building reusable React components and Redux forms. Also, translated wireframes into high quality code." time="June 2017 to October 2017" />
-                    <Experience title="Software Engineer Intern" companyName="Technology for Impact - Refugee Transitions" description="Developed a Salesforce web portal for volunteers of Refugee Transitions using Visualforce and Salesforce Apex.
+                    <Experience title="Software Engineering Intern" companyName="Technology for Impact - Refugee Transitions" description="Developed a Salesforce web portal for volunteers of Refugee Transitions using Visualforce and Salesforce Apex.
 This project improved report completion rate by 25% and saved approximately $90,000 per year." time="May 2015 to July 2015" />
                   </div>
                 </section>
--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,14 @@ function About(){
     return(
       <section className="resume-section p-3 p-lg-5 d-flex d-column" id="about">
         <div className="my-auto">
-          <h1 className="mb-0">Sai Krishna
-            <span className="text-primary">Undurthi</span>
+          <h1 className="mb-0">Hi, I'm
+            <span className="text-primary"> Sai Krishna Undurthi</span>
           </h1>
           <div className="subheading mb-5">810 Gonzalez Drive · San Francisco, CA 94132 · (415) 316-3487 ·
             <a href="mailto:sai.undurthi@gmail.com" title="Email me">sai.undurthi@gmail.com</a>
           </div>
-          <p className="mb-5">Computer Science Graduate with leadership training and experience with academic training at San Francisco State University. Proven skills in Software Engineering, Android Development and Full-Stack Web Development. Experienced in Object oriented methodologies. Well versed in Android mobile technologies with experience using Android Development Tools.
-          Experienced in Java, React JS, NodeJS, HTML, CSS, JavaScript. SQL and NoSQL Databases. Exposure to working in agile work environment. Passionate about learning and using new technologies. Strong analytical, evaluation and interpretation skills. </p>
+          <p className="mb-5">I am a Computer Science Graduate Student(2017) and Software Developer. Experienced in Object oriented methodologies. Well versed in Android, Java, JavaScript, NodeJS and React JS.
+          Used SQL and NoSQL Databases. Exposure to working in agile work environment. Passionate about learning and using new technologies. Currently looking for Internships/Full-Time Opportunities as a Software Developer. </p>
           <ul className="list-inline list-social-icons mb-0">
             <li className="list-inline-item">
               <a href="https://www.linkedin.com/in/saiundurthi/" target="_blank" title="Linkedin">
@@ -30,7 +30,7 @@ function About(){
               </a>
             </li>
             <li className="list-inline-item">
-              <a href="https://drive.google.com/file/d/1JhU4kTb1gMSzU91WN7vwXVIjBo5d-zuT/view?usp=sharing" target="_blank" title="Resume PDF">
+              <a href="https://drive.google.com/file/d/1NL72TRKS_WZAonjgPRS1cf-YX2SRJKh6/view?usp=sharing" target="_blank" title="Resume PDF">
                 <span className="fa-stack fa-lg">
                   <i className="fa fa-circle fa-stack-2x"></i>
                   <i className="fa fa-file-pdf-o fa-stack-1x fa-inverse"></i>
